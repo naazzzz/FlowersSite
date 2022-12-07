3e3fc92e492931f8300331e3257ad4c394981372
artem <artem_berezhnov@mail.ru>
2022-12-07 22:43:49 +0300

    в контроллере добавил управление для продукта, а также реализовал выгрузку из бд таблиц и их вывод

--- controllers/index.php
+++ controllers/index.php
@@ -10,26 +10,95 @@ Class Controller_Index Extends Controller_Base {
 		$this->template->view('main-content');
 	}
 
+    function product() {
+       $id =$_GET["id"];
+       $product=$_GET["table"];
+        $select = array(
+            'where' => 'id = '.$id, // условие
+        );
+        switch ($product){
+            case "homeflower":
+                $model = new Model_Homeflower($select); // создаем объект модели
+                break;
+            case "outflower":
+                $model = new Model_Outflower($select); // создаем объект модели
+                break;
+            case "himiya":
+                $model = new Model_Himiya($select); // создаем объект модели
+                break;
+            case "grunt":
+                $model = new Model_Grunt($select); // создаем объект модели
+                break;
+            case "gorshki":
+                $model = new Model_Gorshki($select); // создаем объект модели
+                break;
+            case "buket":
+                $model = new Model_Buket($select); // создаем объект модели
+                break;
+        }
+
+        $flowersInfo = $model->getAllRows(); // получаем все строки
+        $this->template->vars('flowersInfo', $flowersInfo);
+        $this->template->view('product');
+    }
+
     function homeFlower(){
+        $select = array(
+            'where' => 'id >= 1', // условие
+        );
+        $model = new Model_Homeflower($select); // создаем объект модели
+        $flowersInfo = $model->getAllRows(); // получаем все строки
+        $this->template->vars('flowersInfo', $flowersInfo);
         $this->template->view('home-flower');
+
     }
 
     function outFlower(){
+        $select = array(
+            'where' => 'id >= 1', // условие
+        );
+        $model = new Model_Outflower($select); // создаем объект модели
+        $flowersInfo = $model->getAllRows(); // получаем все строки
+        $this->template->vars('flowersInfo', $flowersInfo);
         $this->template->view('out-flower');
     }
 
     function buket(){
+        $select = array(
+            'where' => 'id >= 1', // условие
+        );
+        $model = new Model_Buket($select); // создаем объект модели
+        $flowersInfo = $model->getAllRows(); // получаем все строки
+        $this->template->vars('flowersInfo', $flowersInfo);
         $this->template->view('buket');
     }
 
     function grunt(){
+        $select = array(
+            'where' => 'id >= 1', // условие
+        );
+        $model = new Model_Grunt($select); // создаем объект модели
+        $flowersInfo = $model->getAllRows(); // получаем все строки
+        $this->template->vars('flowersInfo', $flowersInfo);
         $this->template->view('grunt');
     }
 
     function himiya(){
+        $select = array(
+            'where' => 'id >= 1', // условие
+        );
+        $model = new Model_Himiya($select); // создаем объект модели
+        $flowersInfo = $model->getAllRows(); // получаем все строки
+        $this->template->vars('flowersInfo', $flowersInfo);
         $this->template->view('himiya');
     }
     function gorshki(){
+        $select = array(
+            'where' => 'id >= 1', // условие
+        );
+        $model = new Model_Gorshki($select); // создаем объект модели
+        $flowersInfo = $model->getAllRows(); // получаем все строки
+        $this->template->vars('flowersInfo', $flowersInfo);
         $this->template->view('gorshki');
     }
     function about(){
@@ -38,4 +107,8 @@ Class Controller_Index Extends Controller_Base {
     function aboutAuthor(){
         $this->template->view('about-author');
     }
+
+
+
+
 }
