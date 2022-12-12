710231e020b07605f5383fadfeef956940df5b49
artem <artem_berezhnov@mail.ru>
2022-12-12 17:06:14 +0300

    добавил контроллеры для окна модерации

--- controllers/index.php
+++ controllers/index.php
@@ -7,6 +7,13 @@ Class Controller_Index Extends Controller_Base {
 	
 	// экшен
 	function index() {
+        $select = array(
+            'where' => 'id = 1' // условие
+        );
+
+        $model = new model_content($select); // создаем объект модели
+        $contentInfo = $model->getAllRows(); // получаем все строки
+        $this->template->vars('contentInfo', $contentInfo);
         $this->template->view('mainContent');
 	}
 
@@ -34,9 +41,23 @@ Class Controller_Index Extends Controller_Base {
         $this->template->view($category);
     }
     function about(){
+        $select = array(
+            'where' => 'id = 1' // условие
+        );
+
+        $model = new model_content($select); // создаем объект модели
+        $contentInfo = $model->getAllRows(); // получаем все строки
+        $this->template->vars('contentInfo', $contentInfo);
         $this->template->view('about');
     }
     function aboutAuthor(){
+        $select = array(
+            'where' => 'id = 1' // условие
+        );
+
+        $model = new model_content($select); // создаем объект модели
+        $contentInfo = $model->getAllRows(); // получаем все строки
+        $this->template->vars('contentInfo', $contentInfo);
         $this->template->view('aboutAuthor');
     }
     function moderationAuth(){
@@ -57,11 +78,134 @@ Class Controller_Index Extends Controller_Base {
             $this->template->view('moderation');
         }
     }
-    function moderationSendRequest(){
+
+    function moderationContent(){
         if($_POST["pass"] == 1234){
-        $this->template->view('moderation');
+
+            $modelContent= new model_content();
+            $modelContent->id=1;
+            //обязательно с экранированием
+            $modelContent->mainPage=htmlspecialchars($_POST["mainCont"]);
+            $modelContent->aboutAuthor=htmlspecialchars($_POST["authorCont"]);
+            $modelContent->about=htmlspecialchars($_POST["aboutCont"]);
+            $modelContent->update();
+
+
+            $select = array(
+                'where' => 'id >=1 ' // условие
+            );
+            $model = new model_content($select); // создаем объект модели
+            $contentInfo = $model->getAllRows(); // получаем все строки;
+            $this->template->vars('contentInfo', $contentInfo);
+
+            $model = new model_product($select); // создаем объект модели
+            $flowersInfo = $model->getAllRows(); // получаем все строки;
+            $this->template->vars('flowersInfo', $flowersInfo);
+            $this->template->view('moderation');
         }
     }
 
+    function moderationProductUpdate(){
+        if($_POST["pass"] == 1234){
+
+            $modelProduct = new model_product();
+            $modelProduct->id=$_POST["id"];;
+            $modelProduct->name=$_POST["name"];
+            $modelProduct->price=$_POST["price"];
+            $modelProduct->img=$_POST["img"];
+            $modelProduct->category=$_POST["category"];
+            $modelProduct->description=$_POST["description"];
+            $modelProduct->update();
+
+            $select = array(
+                'where' => 'id >=1 ' // условие
+            );
+            $model = new model_content($select); // создаем объект модели
+            $contentInfo = $model->getAllRows(); // получаем все строки;
+            $this->template->vars('contentInfo', $contentInfo);
+
+            $model = new model_product($select); // создаем объект модели
+            $flowersInfo = $model->getAllRows(); // получаем все строки;
+            $this->template->vars('flowersInfo', $flowersInfo);
+            $this->template->view('moderation');
+        }
+    }
+
+    function moderationProductDelete(){
+        if($_POST["pass"] == 1234) {
+
+            $modelProduct = new model_product();
+            $modelProduct->id=$_POST["id"];;
+            $modelProduct->name=$_POST["name"];
+            $modelProduct->price=$_POST["price"];
+            $modelProduct->img=$_POST["img"];
+            $modelProduct->category=$_POST["category"];
+            $modelProduct->description=$_POST["description"];
+            $modelProduct->deleteRow();
+
+
+            $select = array(
+                'where' => 'id >=1 ' // условие
+            );
+            $model = new model_content($select); // создаем объект модели
+            $contentInfo = $model->getAllRows(); // получаем все строки;
+            $this->template->vars('contentInfo', $contentInfo);
+
+            $model = new model_product($select); // создаем объект модели
+            $flowersInfo = $model->getAllRows(); // получаем все строки;
+            $this->template->vars('flowersInfo', $flowersInfo);
+            $this->template->view('moderation');
+        }
+    }
+
+    function moderationProductAdd(){
+        if($_POST["pass"] == 1234) {
+
+            $modelProduct = new model_product();
+            $modelProduct->name=$_POST["name"];
+            $modelProduct->price=$_POST["price"];
+            $modelProduct->img=$_POST["img"];
+            $modelProduct->category=$_POST["category"];
+            $modelProduct->description=$_POST["description"];
+            $modelProduct->save();
+
+            $select = array(
+                'where' => 'id >=1 ' // условие
+            );
+            $model = new model_content($select); // создаем объект модели
+            $contentInfo = $model->getAllRows(); // получаем все строки;
+            $this->template->vars('contentInfo', $contentInfo);
+
+            $model = new model_product($select); // создаем объект модели
+            $flowersInfo = $model->getAllRows(); // получаем все строки;
+            $this->template->vars('flowersInfo', $flowersInfo);
+            $this->template->view('moderation');
+        }
+    }
+
+    function downloadFile(){
+        if($_POST["pass"] == 1234) {
+            $uploaddir = 'C:/Apache24/htdocs/FlowersSite/img/';
+            $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+            if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+                echo "Файл корректен и был успешно загружен.\n";
+            } else {
+                echo "Файл не был загружен\n";
+            }
+
+
+            $select = array(
+                'where' => 'id >=1 ' // условие
+            );
+            $model = new model_content($select); // создаем объект модели
+            $contentInfo = $model->getAllRows(); // получаем все строки;
+            $this->template->vars('contentInfo', $contentInfo);
+
+            $model = new model_product($select); // создаем объект модели
+            $flowersInfo = $model->getAllRows(); // получаем все строки;
+            $this->template->vars('flowersInfo', $flowersInfo);
+            $this->template->view('moderation');
+        }
+        }
 
 }
